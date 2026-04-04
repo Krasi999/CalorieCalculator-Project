cc172eeb4a6bcf888c68ee16765e08cf4bd376d7
Martin Georgiev <78639036+Martin-Georgiev-21@users.noreply.github.com>
2026-04-04 12:31:36 +0300

    Add project files.

---
+++ CalorieCalculator/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CalorieCalculator")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "CalorieCalculator.Pages")]
