9f2f22387c3ad71850fe8bd5911ed02055611b09
Krasi999 <kgushmakov@tu-sofia.bg>
2026-04-22 10:35:21 +0300

    DTOs is added.

---
+++ API/CalorieCalculator.API/DTOs/DailyCaloriesDTOs.cs
@@ -0,0 +1,12 @@
+﻿public class SaveDailyCaloriesRequest
+{
+    public Guid UserId { get; set; }
+    public DateTime Date { get; set; }
+    public int CaloriesEaten { get; set; }
+}
+
+public class AddDailyCaloriesRequest
+{
+    public Guid UserId { get; set; }
+    public int Calories { get; set; }
+}
