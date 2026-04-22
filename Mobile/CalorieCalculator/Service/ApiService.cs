518028842c695161a68f9496e3c63be39894d6fb
Krasi999 <kgushmakov@tu-sofia.bg>
2026-04-22 11:02:48 +0300

    Test table is created.

--- Mobile/CalorieCalculator/Service/ApiService.cs
+++ Mobile/CalorieCalculator/Service/ApiService.cs
@@ -53,4 +53,6 @@ public class ApiService
 
         return await response.Content.ReadFromJsonAsync<T>();
     }
+
+
 }
--- Mobile/CalorieCalculator/Service/AuthApiService.cs
+++ Mobile/CalorieCalculator/Service/AuthApiService.cs
@@ -179,4 +179,6 @@ public class AuthApiService
     }
 
     private record RegisterResultDto(Guid UserId);
+
+
 }
