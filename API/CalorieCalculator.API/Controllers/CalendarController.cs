fdea7a946fa0add5a029bb771bb38dd15c82c949
Martin Georgiev <78639036+Martin-Georgiev-21@users.noreply.github.com>
2026-04-26 12:13:24 +0300

    calendar linked to calorie plans, calorie plan on creation issue fixed cleared some legacy code

---
+++ API/CalorieCalculator.API/Controllers/CalendarController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+using Services.Queries;
+
+namespace CalorieCalculator.API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CalendarController : ControllerBase
+{
+    private readonly IServices _services;
+
+    public CalendarController(IServices services)
+    {
+        _services = services;
+    }
+
+    [HttpGet("{userId}")]
+    public async Task<IActionResult> GetCalendarData(Guid userId, [FromQuery] int year, [FromQuery] int month)
+    {
+        var result = await _services.Mediator.Send(new CalendarDataQuery
+        {
+            UserID = userId,
+            Year = year,
+            Month = month
+        });
+
+        return Ok(result);
+    }
+}
---
+++ API/Services/Queries/CaloriePlan/CalendarDataQuery.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+
+namespace Services.Queries;
+
+public class CalendarDataQuery : IRequest<CalendarDataResponse>
+{
+    public Guid UserID { get; set; }
+
+    public int Year { get; set; }
+
+    public int Month { get; set; }
+}
+
+public class CalendarDataResponse
+{
+    public List<CalendarDayResponse> Days { get; set; } = new();
+}
+
+public class CalendarDayResponse
+{
+    public DateTime Date { get; set; }
+
+    public int CaloriesGoal { get; set; }
+
+    public int CaloriesEaten { get; set; }
+
+    public bool HasProgram { get; set; }
+}
